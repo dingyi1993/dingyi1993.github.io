200fa0052389c2f27aec6ea899788cab78683bbc
丁一 <dingyi@dingyideMac-mini.local>
2016-09-06 15:23:54 +0800

    Update photo pop.

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,16 +1,21 @@
 require(['jquery', 'layer'], function() {
     $('.entry img').load(function() {
+        console.log($(this).attr('src'));
         var $this = $(this);
-        var data = {
-            alt: $this.attr('alt'),
-            src: $this.attr('src'),
-        };
-        $this.click(function() {
-            layer.photos({
-                photos: {
-                    data: [data]
-                }
+        var src = $this.attr('src');
+        var original = $this.attr('data-original');
+        if (src == original) {
+            var data = {
+                alt: $this.attr('alt'),
+                src: src,
+            };
+            $this.click(function() {
+                layer.photos({
+                    photos: {
+                        data: [data]
+                    }
+                });
             });
-        });
+        }
     });
 });
